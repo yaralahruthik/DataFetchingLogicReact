5c543b8a6a401d2f8fff33639c736ff0961edca1
yaralahruthik <yaralahruthikreddy@gmail.com>
2021-12-06 23:44:10 +0530

    error and loading state set up

--- src/components/ReactWay.js
+++ src/components/ReactWay.js
@@ -2,27 +2,39 @@ import { useEffect, useState } from 'react';
 
 const ReactWay = () => {
 	const [apiData, setApiData] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState('');
 
 	useEffect(() => {
 		const fetchData = async () => {
+			setIsLoading(true);
 			const response = await fetch(
 				'https://jsonplaceholder.typicode.com/todos'
 			);
 			const data = await response.json();
 			setApiData(data);
+			setIsLoading(false);
 		};
 
-		fetchData();
+		fetchData().catch((e) => setError(e.message));
 	}, []);
 
 	return (
 		<div>
 			<h1>React Way</h1>
-			<ul>
-				{apiData.map((item) => (
-					<li key={item.id}>{item.title}</li>
-				))}
-			</ul>
+			{error ? (
+				<>Oh no, there was an error</>
+			) : isLoading ? (
+				<>Loading...</>
+			) : apiData ? (
+				<>
+					<ul>
+						{apiData.map((item) => (
+							<li key={item.id}>{item.title}</li>
+						))}
+					</ul>
+				</>
+			) : null}
 		</div>
 	);
 };
