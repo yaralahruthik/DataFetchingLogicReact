9bd165fe141e7119d438aff2c4b5d535cae462d2
yaralahruthik <yaralahruthikreddy@gmail.com>
2021-12-06 23:56:45 +0530

    Clean up

--- src/components/ReactWay.js
+++ src/components/ReactWay.js
@@ -1,42 +1,42 @@
-import { useEffect, useState } from 'react';
+	import { useEffect, useState } from 'react';
 
-const ReactWay = () => {
-	const [apiData, setApiData] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
-	const [error, setError] = useState('');
+	const ReactWay = () => {
+		const [apiData, setApiData] = useState([]);
+		const [isLoading, setIsLoading] = useState(false);
+		const [error, setError] = useState('');
 
-	useEffect(() => {
-		const fetchData = async () => {
-			setIsLoading(true);
-			const response = await fetch(
-				'https://jsonplaceholder.typicode.com/todos'
-			);
-			const data = await response.json();
-			setApiData(data);
-			setIsLoading(false);
-		};
+		useEffect(() => {
+			const fetchData = async () => {
+				setIsLoading(true);
+				const response = await fetch(
+					'https://jsonplaceholder.typicode.com/todos'
+				);
+				const data = await response.json();
+				setApiData(data);
+				setIsLoading(false);
+			};
 
-		fetchData().catch((e) => setError(e.message));
-	}, []);
+			fetchData().catch((e) => setError(e.message));
+		}, []);
 
-	return (
-		<div>
-			<h1>React Way</h1>
-			{error ? (
-				<>Oh no, there was an error</>
-			) : isLoading ? (
-				<>Loading...</>
-			) : apiData ? (
-				<>
-					<ul>
-						{apiData.map((item) => (
-							<li key={item.id}>{item.title}</li>
-						))}
-					</ul>
-				</>
-			) : null}
-		</div>
-	);
-};
+		return (
+			<div>
+				<h1>React Way</h1>
+				{error ? (
+					<>Oh no, there was an error</>
+				) : isLoading ? (
+					<>Loading...</>
+				) : apiData ? (
+					<>
+						<ul>
+							{apiData.map((item) => (
+								<li key={item.id}>{item.title}</li>
+							))}
+						</ul>
+					</>
+				) : null}
+			</div>
+		);
+	};
 
-export default ReactWay;
+	export default ReactWay;
